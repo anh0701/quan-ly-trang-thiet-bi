03860938c3c24f0d0db4e628dcde7509123d0239
duyetnguyen-n <104747808+duyetnguyen-n@users.noreply.github.com>
2023-03-29 21:46:31 +0700

    duyet update

---
+++ NCKH/FrmThemTTBVaoCase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NCKH
+{
+    public partial class FrmThemTTBVaoCase : Form
+    {
+        public FrmThemTTBVaoCase()
+        {
+            InitializeComponent();
+            loaddtgvChiTietTrangThietBiCoTrongCase();
+        }
+        private void loaddtgvChiTietTrangThietBiCoTrongCase()
+        {
+            string strQuery = "SELECT * FROM ChiTietTrangThietBiCoTrongCase";
+            Dictionary<string, object> parameters = new Dictionary<string, object>();
+            DataTable table = Database.Query(strQuery, parameters);
+            dataGridView1.DataSource = table;
+
+        }
+        private void FrmThemTTBVaoCase_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
